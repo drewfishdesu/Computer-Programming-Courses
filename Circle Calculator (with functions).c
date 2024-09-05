b00d7713932e60cbd42f9c6ed8d36e5e882fed4b
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-09-05 09:00:50 +0800

    Unfinished

---
+++ Circle Calculator (with functions).c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+float circleArea(); // Function Prototype
+float circleArea() // Function Header
+{
+    // Function Body
+    float radius = (dm / 2)
+    float comp = (PI * radius * radius);
+}
+
+int main()
+{
+    // Declare variables.
+
+    float dm;
+    float comp;
+    float radius;
+    float value;
+    int choice;
+
+    // Asks user which calculation to do.
+	// 1 for area; 2 for circumference.
+
+    scanf("%d %f", &choice, &dm);
+
+    if(choice == 1)
+    {
+        value = circleArea
+        printf("value")
+    }
+}
