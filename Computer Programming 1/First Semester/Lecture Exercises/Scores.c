3bc2b0713560d65bf1068ef61900fd5207562a5b
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-09-13 14:15:53 +0800

    Added Scores.c

---
+++ Computer Programming 1/First Semester/Lecture Exercises/Scores.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void printMessage(int score); // Function prototype
+void printMessage(int score) // Function header
+{
+	// If-else statements
+    if(score >= 95 && score <= 100)
+    {
+        printf("Excellent!");
+    }
+    else if(score >= 85 && score <= 94)
+    {
+        printf("You did well!");
+    }
+    else if(score >= 75 && score <= 84)
+    {
+        printf("You did fine!");
+    }
+    else if(score >= 60 && score <= 74)
+    {
+        printf("You passed!");
+    }
+    else if(score > 100)
+    {
+        printf("Invalid score!");
+    }
+    else
+    {
+        printf("Try harder next time!");
+    }
+}
+
+int main()
+{
+    // Declare variables
+    int score;
+    
+    scanf("%d", &score);
+	
+	// Calls the printMessage function.
+	
+	printMessage(score);
+
+return 0;
+
+}
