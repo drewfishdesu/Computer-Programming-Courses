e270c6d4a93138fc0391be904c66c2293444dcd8
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-09-13 13:36:56 +0800

    Added C version of Artifact Calculator

---
+++ Side Projects/C/Genshin Artifact Crit Value Calculator/Artifact CV.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+
+// Declare variables
+{
+    float crit_damage;
+    float crit_rate;
+    float crit_value;
+
+    // Prints a message indicating that the program is functioning.
+
+    printf("\n");
+    printf("Welcome to the artifact calculator. Input your crit damage followed by the crit rate.");
+    printf("\n");
+    printf("Example: 21 10.5");
+
+    // Receives input from the user (crit damage, crit rate)
+
+    printf("\n");
+    scanf("%f %f", &crit_damage, &crit_rate);
+
+    // Crit value calculation
+
+    crit_value = ((crit_rate * 2) + crit_damage);
+
+    // Prints the crit value for your artifact.
+
+    printf("Your artifact has %.2f CV.", crit_value);
+    return 0;
+
+}
