1cf305a2f3eae5ed90df68c4496b152f0e551c3c
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-09-25 11:30:12 +0800

    New project

---
+++ Side Projects/C++/Genshin Artifact Calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Define variables
+    float crit_damage;
+    float crit_rate;
+    float crit_value;
+
+    cout << "Welcome to the Genshin Artifact Calculator" << endl;
+    cout << "To get started, type your CRIT Rate followed by your CRIT DMG." << endl;
+    cout << "For example, 21.8 14.4" << endl;
+
+    // Asks the user for input.
+
+    cin >> crit_rate >> crit_damage;
+
+    // Calculates the crit value of an artifact.
+    crit_value = (crit_rate * 2) + crit_damage;
+    
+    // Prints the crit value of an artifact.
+    cout << fixed << setprecision(2);
+    cout << "Your artifact crit value is: " << crit_value << endl;
+    return 0;
+}
