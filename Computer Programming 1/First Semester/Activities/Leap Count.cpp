96ba4b2e44566a2f25401ebff5d055d270238d47
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-10-03 08:38:36 +0800

    Update

--- Computer Programming 1/First Semester/Activities/Leap Count.cpp
+++ Computer Programming 1/First Semester/Activities/Leap Count.cpp
@@ -19,7 +19,7 @@ int main()
     while (year <= currentYear)
     {
         // Checks if the year is a leap year.
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        if (year % 4 == 0)
         {
             leapYearCount++;
         }
