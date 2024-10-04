5c6fe626d7119b317816bea590e0469225ecd56b
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-10-04 12:44:09 +0800

    Update

--- Computer Programming 1/First Semester/Activities/C++/Factors.cpp
+++ Computer Programming 1/First Semester/Activities/C++/Factors.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    int count = 1;
+    int x = number;
+
+    cin >> number << endl;
+
+    while(count <= x) // Uses a while loop to find and print factors.
+    {
+        if(number % count == 0) // Check if count is a factor of a number.
+        {
+            cout << count << " " << endl; // Prints the factor
+        }
+        count++; // Moves to the next integer
+    }
+    
+    cout << endl; // Prints a new line at the end
+    return 0;
+
+}
