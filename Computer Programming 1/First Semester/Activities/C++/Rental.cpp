ffe42b52e4edf003f7158d1287444f10a6f1b8f7
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-10-11 13:11:22 +0800

    Update

---
+++ Computer Programming 1/First Semester/Activities/C++/Rental.cpp
@@ -0,0 +1,27 @@
+#include <iosteam>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int tot_VCD;
+    int num_days;
+    float current_rental
+    float total_rental = 0;
+
+    cin >> tot_VCD;
+    cin >> num_days;
+
+    int i:
+    for(i = 0; i < tot_VCD; i++)
+    {
+        cin << current_rental
+        total_rental += current_rental;
+    }
+
+    total_rental *= num_days;
+
+    cout << fixed << setpricision(2) << total_rental;
+
+    return 0;
+ }
