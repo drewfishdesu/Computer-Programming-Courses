f2ca3e0b31ab956ca02da263c88ff163a613a970
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-10-11 13:27:34 +0800

    Update

--- Computer Programming 1/First Semester/Activities/C++/Rental.cpp
+++ Computer Programming 1/First Semester/Activities/C++/Rental.cpp
@@ -1,27 +1,33 @@
-#include <iosteam>
+#include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
-    int tot_VCD;
-    int num_days;
-    float current_rental
-    float total_rental = 0;
+    
+    // Variable declaration
+    int tot_VCD; // Total number of VCDs rented
+    int num_days; // Loan time amount (in days)
+    float current_rental; // Rental price of a VCD
+    float total_rental = 0; // Float to accumulate total rental cost
 
+    // Asks for user input
     cin >> tot_VCD;
     cin >> num_days;
 
-    int i:
-    for(i = 0; i < tot_VCD; i++)
-    {
-        cin << current_rental
+    int i;
+    for(i = 0; i < tot_VCD; i++) // Loop for Rental Calculation
+    {   
+        // Asks for the current rental price of a VCD
+        cin >> current_rental;
         total_rental += current_rental;
     }
 
+    // Total rental cost is multiplied by number of days
     total_rental *= num_days;
 
-    cout << fixed << setpricision(2) << total_rental;
+    // Outputs the final price of all rented VCDs for x number of days.
+    cout << fixed << setprecision(2) << total_rental;
 
     return 0;
  }
