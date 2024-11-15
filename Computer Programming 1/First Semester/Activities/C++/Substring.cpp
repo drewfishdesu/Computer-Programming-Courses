ffe33abb3996c41dc2f34b8d27d25ca50322f80a
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-11-15 14:12:48 +0800

    Added substring.cpp

---
+++ Computer Programming 1/First Semester/Activities/C++/Substring.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int size = 30;
+    char word[size]
+
+    int start;
+    int endlength;
+    int looplength;
+
+    cin.get(a, size)
+    cin >> start >> endlength;
+    looplength = start  - 1 + endlength;
+    for(int x = start - 1; x < looplength; x++)
+    {
+        cout << word[x]
+    }
+
+    return 0;
+
+}
