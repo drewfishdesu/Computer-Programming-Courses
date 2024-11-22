7fc0d3999a999d86a1530a1ff693c0e8b844291c
Andrew Sacriz <31579293+drewfishdesu@users.noreply.github.com>
2024-11-22 14:35:36 +0800

    Update

--- Computer Programming 1/First Semester/Activities/C++/Vowels (Primitive).cpp
+++ Computer Programming 1/First Semester/Activities/C++/Vowels (Primitive).cpp
@@ -6,7 +6,7 @@ int main()
 {
     char word[50];
     int vowelCount = 0;
-    char vowels[10] = {'a', 'i', 'e', 'o', 'u', 'A', 'I', 'E', 'O', 'U'} 
+    char vowels[10] = {'a', 'i', 'e', 'o', 'u', 'A', 'I', 'E', 'O', 'U'};
     
     cin.get(word, 50);
     for(int i = 0; i < strlen(word); i++)
